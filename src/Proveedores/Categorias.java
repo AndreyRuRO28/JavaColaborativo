8acceb4dd1f8c47bb8f820fff0810b7b492327d8
AndreyRuRO28 <anrugamaro@est.utn.ac.cr>
2023-11-24 10:28:09 -0600

    Clase categorias
    Se crea la clase categorias

---
+++ src/Proveedores/Categorias.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proveedores;
+ 
+/**
+ *
+ * @author Andrey Rugama
+ */
+public class Categorias {
+    
+}
+ 
